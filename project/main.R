d8d8bf652f97af99f2faa3690dc2c1cb3ddec1a3
Tuomo Artama <tuomo.artama@gmail.com>
2022-12-05 15:23:59 +0200

    first commit

--- project/main.R
+++ project/main.R
@@ -1,1 +1,15 @@
-a <-0
+npf <- read.csv("npf_train.csv")
+
+# Remove columns "id", "date" and "partlybad" and set dates as rownames
+rownames(npf) <- npf[, "date"]
+npf <- npf[, -c((1:2), 4)]
+
+# Create variable class2 where the value is "event" if there was an NPF event
+# and "nonevent" otherwise.
+npf$class2 <- factor("event", levels = c("nonevent", "event"))
+npf$class2[npf$class4 == "nonevent"] <- "nonevent"
+
+# Remove variable class4 because predicting will be done on variable class2
+npf <- npf[, -1]
+
+summary(npf)
